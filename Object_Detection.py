1fcbe5a7d131f8016f2aa152ebeb5e6e277f6c3e
Lekha Ladhe <ladhelekha@gmail.com>
2022-10-14 10:10:54 +0530

    added detection logic👌

--- Object_Detection.py
+++ Object_Detection.py
@@ -26,6 +26,35 @@ while True:
     class_ids = []
 
     # object detection logic to be written here
+    for output in layerOutputs:
+        for detection in output:
+            scores = detection[5:]
+            class_id = np.argmax(scores)
+            confidence = scores[class_id]
+            if confidence > 0.2:
+                center_x = int(detection[0]*width)
+                center_y = int(detection[1]*height)
+                w = int(detection[2]*width)
+                h = int(detection[3]*height)
+
+                x = int(center_x - w/2)
+                y = int(center_y - h/2)
+
+                boxes.append([x, y, w, h])
+                confidences.append((float(confidence)))
+                class_ids.append(class_id)
+
+    indexes = cv2.dnn.NMSBoxes(boxes, confidences, 0.2, 0.4)
+
+    if len(indexes) > 0:
+        for i in indexes.flatten():
+            x, y, w, h = boxes[i]
+            label = str(classes[class_ids[i]])
+            confidence = str(round(confidences[i], 2))
+            color = colors[i]
+            cv2.rectangle(img, (x, y), (x+w, y+h), color, 2)
+            cv2.putText(img, label + " " + confidence,
+                        (x, y+20), font, 2, (255, 255, 255), 2)
 
     cv2.imshow('Image', img)
     key = cv2.waitKey(1)
